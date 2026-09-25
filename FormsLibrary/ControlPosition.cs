395e9bc971a9212870484a7960a3ef6507485b77
Thijs Bruinenberg <thijsbruinenberg2002@gmail.com>
2026-09-25 10:49:09 +0200

    Base of the library is set.

---
+++ FormsLibrary/ControlPosition.cs
@@ -0,0 +1,10 @@
+﻿namespace FormsLibrary
+{
+    public class ControlPosition
+    {
+        public Point calcMiddle(Control control)
+        {
+            return new Point(control.Width/2, control.Height/2);
+        }
+    }
+}
